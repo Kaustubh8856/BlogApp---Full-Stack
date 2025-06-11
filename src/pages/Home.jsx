288ee2e37bb51c8013cfb7de483d81aed5340547
Kaustubh <kaustubh8856@gmail.com>
2025-06-11 21:14:46 +0530

    implement blog list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Header from "../Components/Header";
+import BlogList from "../Components/BlogList";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <BlogList />
     </>
   );
 };
