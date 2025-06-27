bf8e0d993a8f10f0a23f7f1ffe4dd3cc10cbe930
Kaustubh <kaustubh8856@gmail.com>
2025-06-27 19:14:34 +0530

    implement login functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,17 @@ import Comments from "./pages/admin/Comments";
 import ListBlog from "./pages/admin/ListBlog";
 import Login from "./Components/admin/Login";
 import "quill/dist/quill.snow.css";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./Context/AppContext";
 const App = () => {
+  const { token } = useAppContext();
   return (
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+        <Route path="/admin" element={token ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="comments" element={<Comments />} />
--- client/src/Components/admin/Login.jsx
+++ client/src/Components/admin/Login.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
+import { useAppContext } from "../../Context/AppContext";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const { axios, setToken } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/admin/login", { email, password });
+      if (data.success) {
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        axios.defaults.headers.common["Authorization"] = data.token;
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
   return (
     <div className="flex items-center justify-center h-screen">
@@ -14,14 +29,9 @@ const Login = () => {
             <h1 className="text-3xl font-bold">
               <span className="text-primary">Admin</span> Login.
             </h1>
-            <p className="font-light">
-              Enter your credentials to axcess the admin panel.
-            </p>
+            <p className="font-light">Enter your credentials to axcess the admin panel.</p>
           </div>
-          <form
-            onSubmit={handleSubmit}
-            className="mt-6 w-full sm:max-w-md text-gray-600"
-          >
+          <form onSubmit={handleSubmit} className="mt-6 w-full sm:max-w-md text-gray-600">
             <div className="flex flex-col">
               <label>Email</label>
               <input
@@ -44,10 +54,7 @@ const Login = () => {
                 required
               />
             </div>
-            <button
-              type="submit"
-              className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all"
-            >
+            <button type="submit" className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all">
               Submit
             </button>
           </form>
